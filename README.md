c50acdbd57d980239d281378dd3e788ba49c43cb
makikovaughan <makiko.vaughan@gmail.com>
2023-09-03 05:46:39 -0400

    Upload Resume Templater and update README file

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # ASP_Study
+
+## These files were all projects for Codecademy ASP.NET course
+1. Resume Templater(/ResumeTemplater): 
+	The Index page will contain data 
+regarding your experience, and technical skills about what programming 
+languages you know.
+	The Projects page will contain information about the projects you have 
+built!
---
+++ ResumeTemplater/ResumeTemplater/Pages/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿// Index.cshtml.cs
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace ResumeTemplater.Pages
+{
+    public class IndexModel : PageModel
+    {
+        //Properties
+        public string FullName { get; set; }
+        public string LinkedInUsername { get; set; }
+        public int YearsOfExperience { get; set; }
+        public List<string> Languages { get; set; }
+        public string GithubUsername { get; set; }
+
+        //Method
+        public void OnGet()
+        {
+            FullName = "Makiko Vaughan";
+            LinkedInUsername = "makiko-vaughan";
+            YearsOfExperience = 8;
+            Languages = new List<string> { "JavaScript", "Java", "HTML" };
+            GithubUsername = "makikovaughan";
+        }
+    }
+}
